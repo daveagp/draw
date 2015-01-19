f28eaa18310312a2a37e0f16bb90b9ec8e9c7a6f
David Pritchard <daveagp@gmail.com>
2015-01-18 17:47:31 -0800

    remove need for second implementation file

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ clean:
 
 # compile lib with g++ since clang++ has issues with std::{thread,chrono}
 lib:	
-	moc-qt4 draw_qo.h | g++ -g -c -std=c++11 -I/usr/include/qt4/QtGui -I/usr/include/qt4 -x c++ - -include draw.cpp -o draw.o
+	moc-qt4 draw.cpp | g++ -g -c -std=c++11 -I/usr/include/qt4/QtGui -I/usr/include/qt4 -x c++ - -include draw.cpp -o draw.o
 
 example: lib
 	g++ example.cpp draw.o -lQtGui -lQtCore -o example
--- draw.cpp
+++ draw.cpp
@@ -1,5 +1,4 @@
 #include "draw.h"
-#include "draw_qo.h"
 #include <QPainter>
 #include <QApplication>
 #include <QPixmap>
@@ -8,6 +7,39 @@
 #include <atomic>
 #include <chrono>
 #include <iostream>
+
+#include <QWidget>
+
+class WindowWidget : public QWidget
+{
+  Q_OBJECT  
+  public:
+    WindowWidget(QWidget *parent = 0);
+    ~WindowWidget();
+  protected:
+    void paintEvent(QPaintEvent *event);
+  public slots:
+    void catchEllipse(int, int, int, int);
+    void catchRepaint();
+    void catchClear();
+  private:
+    QPixmap* pm;
+};
+
+class APIWidget : public QObject
+{
+  Q_OBJECT
+  public:
+    void sendEllipsePub(int, int, int, int);
+    void sendRepaintPub();
+    void sendClearPub();
+  signals:
+    void sendEllipse(int, int, int, int);
+    void sendRepaint();
+    void sendClear();
+};
+
+
 using namespace std;
 
 atomic<APIWidget*> aw;
