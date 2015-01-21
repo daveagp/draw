c821017bd82019137646fca772e48f991f7cb793
David Pritchard <daveagp@gmail.com>
2015-01-21 10:12:11 -0800

    coords

--- bull.cpp
+++ bull.cpp
@@ -1,11 +1,10 @@
-#include "draw.h"
+#include "draw.h"                      // you have to include this
 
 int main() {
-   draw::setcolor(255, 0, 0);
-   draw::filled_circle(0.5, 0.5, 0.5);
-   draw::setcolor(255, 255, 255);
-   draw::filled_circle(0.5, 0.5, 0.3);
-   draw::setcolor(draw::RED);
-   draw::filled_circle(0.5, 0.5, 0.1);   
-   draw::save("bull.png");
+   draw::setcolor(255, 0, 0);          // fully red, no green or blue
+   draw::filled_circle(0.5, 0.5, 0.5); // center 0.5, 0.5 and radius 0.5
+   draw::setcolor(255, 255, 255);      // white
+   draw::filled_circle(0.5, 0.5, 0.3); // smaller circle
+   draw::setcolor(draw::RED);          // predefined alias for red
+   draw::filled_circle(0.5, 0.5, 0.1); // smaller circle
 }
---
+++ coords.cpp
@@ -0,0 +1,20 @@
+#include "draw.h"
+
+int main() {
+   draw::text("(0, 0)", 0.05, 0.05);
+   draw::text("(0, 1)", 0.05, 0.95);
+   draw::text("(1, 0)", 0.95, 0.05);
+   draw::text("(1, 1)", 0.95, 0.95);
+   draw::text("x-axis", 0.5, 0.15);
+   draw::text("y-axis", 0.05, 0.5);
+
+   draw::line(0.1, 0.1, 0.1, 0.9);
+   draw::line(0.0, 0.8, 0.1, 0.9); 
+   draw::line(0.2, 0.8, 0.1, 0.9);
+
+   draw::line(0.1, 0.1, 0.9, 0.1);
+   draw::line(0.8, 0.0, 0.9, 0.1);
+   draw::line(0.8, 0.2, 0.9, 0.1);
+   
+   draw::save("media/coords.png");
+}
