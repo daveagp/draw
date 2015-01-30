5a7ff7290f29ec4765ac8ba6760caf66d19c676c
Dave Pritchard <daveagp@gmail.com>
2015-01-30 13:40:53 -0800

    fix ridiculous memory leak. props to Chih-Fan Chen!

--- draw.cpp
+++ draw.cpp
@@ -245,6 +245,7 @@ Q_INVOKABLE void showframe() {
       animation_mode = true;
    }
    else {
+      delete prepared_frame;
       prepared_frame = new QPixmap(*pm);
       QPainter p(prepared_frame);
       p.drawPixmap(0, 0, *pm);
