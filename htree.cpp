d674de62d4159e3513540b9bf46b56788ec612eb
Dave Pritchard <daveagp@gmail.com>
2015-03-17 19:24:35 -0700

    add comments to recursive examples

--- htree.cpp
+++ htree.cpp
@@ -1,17 +1,32 @@
+/*
+recursive program to draw an H-tree.
+compile: make htree
+run: htree 10 (or any other number)
+*/
+
 #include "draw.h"
 #include <cstdlib>
 
+// recursive function to draw a level-n H-tree centered at x, y
 void htree(int n, double size, double x, double y) {
-   if (n == 0) return;
+   // base case
+   if (n == 0)
+      return;
+
+   // recursive case:
+
+   // compute coordinates of corners of this H
    double x0 = x - size/2;
    double x1 = x + size/2;
    double y0 = y - size/2;
    double y1 = y + size/2;
 
+   // draw the H
    draw::line(x0, y, x1, y);
    draw::line(x0, y0, x0, y1);
    draw::line(x1, y0, x1, y1);
-        
+
+   // draw 4 smaller H-trees
    htree(n-1, size/2, x0, y0);
    htree(n-1, size/2, x0, y1);
    htree(n-1, size/2, x1, y0);
@@ -20,7 +35,11 @@ void htree(int n, double size, double x, double y) {
 
 int main(int argc, char* argv[]) {
    int n = 5;
-   if (argc > 1) n = atoi(argv[1]);
+   if (argc > 1)
+      n = atoi(argv[1]);
+   
+   // start the recursion
    htree(n, .5, .5, .5);
+   
    return 0;
 }
--- nestedcircles.cpp
+++ nestedcircles.cpp
@@ -7,25 +7,31 @@ void fancyCircle(double x, double y, double r) {
    bool randomBit = (random() % 2) == 0; 
    // this is the color that will fill the circle
    if (randomBit == 0)
-      draw::setcolor(draw::RED);
+      draw::setcolor(draw::CHARTREUSE);
    else 
-      draw::setcolor(draw::YELLOW);
+      draw::setcolor(draw::VIOLET);
    draw::filled_circle(x, y, r);
    // this is the color for the circle's border
    if (randomBit == 0)
-      draw::setcolor(draw::VIOLET);
+      draw::setcolor(draw::OLIVE);
    else
       draw::setcolor(draw::TEAL);
    draw::circle(x, y, r);
 }
 
-// draw an order-n nested circle, centred on (x, y) with radius r
+// draw an order-n nested circle, centered on (x, y) with radius r
 void ncirc(int n, double x, double y, double r) {
-   if (n==0) return;
+   // base case
+   if (n==0)
+      return;
+      
+   // recursive case:
+
+   // draw one circle
    fancyCircle(x, y, r);
 
-   double halfRadius = r/2;
    // recursively draw two nested circles of order n-1
+   double halfRadius = r/2;
    ncirc(n-1, x - halfRadius, y, halfRadius);
    ncirc(n-1, x + halfRadius, y, halfRadius);
 }
@@ -33,7 +39,8 @@ void ncirc(int n, double x, double y, double r) {
 // read in a command-line argument N and plot an order-N circle
 int main(int argc, char* argv[]) {
    int n = 5;
-   if (argc > 1) n = atoi(argv[1]);
+   if (argc > 1)
+      n = atoi(argv[1]);
    ncirc(n, .5, .5, .5);
    return 0;
 }
